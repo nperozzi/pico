aea9380eb4bf5fc0b831251ce333e96e891f1ba3
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-04-01 16:18:12 +0000

    Removed build folder

--- build/blink/CMakeFiles/blink.dir/home/dev/ws/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o.d
+++
@@ -1,2 +0,0 @@
-blink/CMakeFiles/blink.dir/home/dev/ws/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o: \
- /home/dev/ws/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c
